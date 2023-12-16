66a5414a8c0be9abf5f06ccd5c8996ee83854c1f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-12-16 13:49:16 -0500

    0.0.4

--- types.d.ts
+++ types.d.ts
@@ -7,11 +7,11 @@ export interface MountInit{
     readonly whereSatisfies?: PipelineProcessor<boolean>,
     readonly import?: ImportString | [ImportString, ImportAssertions] | PipelineProcessor,
     readonly do?: {
-        readonly onMount: PipelineProcessor,
-        readonly onDismount: PipelineProcessor,
-        readonly onDisconnect: PipelineProcessor,
-        readonly onReconfirmed: PipelineProcessor,
-        readonly onOutsideRootNode: PipelineProcessor,
+        readonly onMount?: PipelineProcessor,
+        readonly onDismount?: PipelineProcessor,
+        readonly onDisconnect?: PipelineProcessor,
+        readonly onReconfirmed?: PipelineProcessor,
+        readonly onOutsideRootNode?: PipelineProcessor,
     }
     
 }
