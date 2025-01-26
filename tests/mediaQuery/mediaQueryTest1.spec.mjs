4217421407ada1bec6e7c78a72d7267fe922b3fb
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-01-26 14:52:14 -0500

    0.0.40

--- tests/mediaQuery/mediaQueryTest1.spec.mjs
+++ tests/mediaQuery/mediaQueryTest1.spec.mjs
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 test('mediaQueryTest1', async ({ page }) => {
     
-    await page.goto('./tests/mediaQueryTest1.html');
-    await page.setViewportSize({ width: '400px', height: '600px' });
+    await page.goto('./tests/mediaQuery/mediaQueryTest1.html');
+    await page.setViewportSize({ width: 400, height: 600 });
     // wait for 1 second
     await page.waitForTimeout(1000);
     const editor = page.locator('#target');
