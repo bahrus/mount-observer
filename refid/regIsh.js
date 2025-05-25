fa5fc42c5a95085719b9803ae494edd3ef897262
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-05-25 15:23:30 -0400

    0.0.65

--- refid/regIsh.js
+++ refid/regIsh.js
@@ -23,5 +23,11 @@ export function regIsh(scope, name, ctr) {
             }
         };
     };
+    ctr.prototype['#arr='] = function (newArr) {
+        if (newArr === undefined) {
+            return this[arr];
+        }
+        this[arr] = newArr;
+    };
     document.dispatchEvent(new Event(guid));
 }
