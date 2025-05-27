a3158158221cdbae1db64b19556cef346ce98d4e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-05-27 06:40:59 -0400

    0.0.68

--- tests/ish/eventCheck.spec.mjs
+++ tests/ish/eventCheck.spec.mjs
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 test('eventCheck', async ({ page }) => {
     await page.goto('./tests/ish/eventCheck.html');
-    // wait for 1 second
-    await page.waitForTimeout(1000);
+    // wait for 5 seconds
+    await page.waitForTimeout(5000);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
 });
