06ab3f6b7b264747fa35142b0de67ea17663b756
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-06 03:56:48 -0400

    Merge branch 'baseline' of https://github.com/bahrus/mount-observer into baseline

--- slotkin/kin.ts
+++ slotkin/kin.ts
@@ -1,4 +1,5 @@
 import {toQuery} from './toQuery.js';
+import {splitRefs} from '../refid/splitRefs.js';
 export function kin(fragment: DocumentFragment, el: Element){
     const qry = toQuery(el);
     const matches = Array.from(fragment.querySelectorAll(qry));
@@ -6,6 +7,15 @@ export function kin(fragment: DocumentFragment, el: Element){
     const clone = el.cloneNode(true);
     for(const child of clone.childNodes){
         elFragment.appendChild(child);
+    }
+    const insertAttrs = el.getAttribute('-i');
+    let map: {[key: string]: string} | null = null;
+    if(insertAttrs !== null){
+        const attrs = splitRefs(insertAttrs);
+        map = {};
+        for(const attr of attrs){
+            map[attr] = el.getAttribute(attr)!;
+        }
     }
     for(const match of matches){
         const fragmentClone = elFragment.cloneNode(true) as DocumentFragment;
