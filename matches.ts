6dab460142dd94ab4e42868356b9b5a1dd6da0ba
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-06 03:06:16 -0400

    Merge branch 'baseline' of https://github.com/bahrus/mount-observer into baseline

--- matches.ts
+++ matches.ts
@@ -3,7 +3,7 @@ export function toQuery(el: Element): string {
     //from the element el, create a selector that matches all the classes and parts of the element el, as 
     //well as the values of all the attributes of el.
     const classes = Array.from(el.classList).map(c => `.${c}`).join('');
-    const parts = Array.from(el.part).map(p => `::part(${p})`).join('');
+    const parts = Array.from(el.part).map(p => `[part~="${p}"`).join('');
     const attributes = Array.from(el.attributes)
         .map(attr => `[${attr.name}="${attr.value}"]`)
         .join('');
