d1206f3e548932e0ac64cd46f733c3aca82db524
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-06 03:04:18 -0400

    Merge branch 'baseline' of https://github.com/bahrus/mount-observer into baseline

--- matches.ts
+++ matches.ts
@@ -1,13 +1,12 @@
 
-export function matches(lhs: Element, rhs: Element){
-    //lhs matches rhs if all the css classes of lhs are found in the classList of rhs,
-    //and all the parts of lhs are found in the part of rhs,
-    //and all the other attributes of lhs are found in the attributes of rhs.
-    if(lhs.classList.length !== 0 && !rhs.classList.contains(...lhs.classList)) return false;
-    if(lhs.part.length !== 0 && !rhs.part.contains(...lhs.part)) return false;
-    for(const attr of lhs.attributes){
-        if(attr.name === 'class' || attr.name === 'part') continue; //already checked
-        if(!rhs.hasAttribute(attr.name)) return false;
-        if(rhs.getAttribute(attr.name) !== attr.value) return false;
-    }
+export function toQuery(el: Element): string {
+    //from the element el, create a selector that matches all the classes and parts of the element el, as 
+    //well as the values of all the attributes of el.
+    const classes = Array.from(el.classList).map(c => `.${c}`).join('');
+    const parts = Array.from(el.part).map(p => `::part(${p})`).join('');
+    const attributes = Array.from(el.attributes)
+        .map(attr => `[${attr.name}="${attr.value}"]`)
+        .join('');
+    const tagName = el.tagName.toLowerCase();
+    return `${tagName}${classes}${parts}${attributes}`;
 }
