093dc3404d9584a598f688f482afea080f1ed6a8
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-07 08:53:12 -0400

    Merge branch 'baseline' of https://github.com/bahrus/mount-observer into baseline

--- slotkin/beKindred.ts
+++ slotkin/beKindred.ts
@@ -1,6 +1,7 @@
 import {toQuery} from './toQuery.js';
 import {splitRefs} from '../refid/splitRefs.js';
-export function beKindred(fragment: DocumentFragment, el: Element){
+
+export function beKindred(fragment: DocumentFragment | Element, el: Element){
     const qry = toQuery(el);
     const matches = Array.from(fragment.querySelectorAll(qry));
     const elFragment = new DocumentFragment();
