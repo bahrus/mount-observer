2dfd709ad26146d162dd9f3d4df20be8e22eabca
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-06-14 12:58:37 -0400

    0.0.71

--- refid/getAdjRefs.js
+++ refid/getAdjRefs.js
@@ -1,16 +1,37 @@
 import { splitRefs } from './splitRefs.js';
-export function getAdjRefs(el) {
-    const returnArr = [el];
-    const itemref = el.getAttribute('itemref');
-    if (itemref === null)
-        return returnArr;
-    const itemrefList = splitRefs(itemref); // itemref.split(' ').map((id) => id.trim()).filter((id) => id.length > 0);
-    if (itemrefList.length === 0)
-        return returnArr;
-    let ns = el.nextElementSibling;
-    while (ns !== null && itemrefList.includes(ns.id)) {
-        returnArr.push(ns);
-        ns = ns.nextElementSibling;
+export function getAdjRefs(node) {
+    const returnArr = [node];
+    if (node.nodeType === node.COMMENT_NODE) {
+        const closedText = `/${node.data}`;
+        let ns = node.nextSibling;
+        while (ns) {
+            returnArr.push(node);
+            if (node.nodeType === node.COMMENT_NODE && node.data === closedText) {
+                return returnArr;
+            }
+            ns = ns.nextSibling;
+        }
+    }
+    else {
+        const el = node;
+        const itemref = el.getAttribute('itemref');
+        if (itemref === null)
+            return returnArr;
+        const itemrefList = splitRefs(itemref); // itemref.split(' ').map((id) => id.trim()).filter((id) => id.length > 0);
+        if (itemrefList.length === 0)
+            return returnArr;
+        let ns = el.nextSibling;
+        while (ns !== null) {
+            if (ns instanceof Element) {
+                if (ns.id && itemrefList.includes(ns.id)) {
+                    returnArr.push(ns);
+                }
+            }
+            else {
+                return returnArr;
+            }
+            ns = ns.nextSibling;
+        }
     }
     return returnArr;
 }
--- refid/getAdjRefs.ts
+++ refid/getAdjRefs.ts
@@ -1,15 +1,36 @@
 import { splitRefs } from './splitRefs.js';
-export function getAdjRefs(el: Element): Array<Element>{
-    const returnArr = [el];
-    const itemref = el.getAttribute('itemref');
-    if(itemref === null) return returnArr;
-    const itemrefList = splitRefs(itemref);// itemref.split(' ').map((id) => id.trim()).filter((id) => id.length > 0);
-    if(itemrefList.length === 0) return returnArr;
-    let ns = el.nextElementSibling;
-    while(ns !== null && itemrefList.includes(ns.id)){
-        returnArr.push(ns);
-        ns = ns.nextElementSibling;
+export function getAdjRefs(node: Node){
+    const returnArr: Array<Node> = [node];
+    if(node.nodeType === node.COMMENT_NODE){
+        const closedText = `/${(node as Comment).data}`;
+        let ns = node.nextSibling;
+        while(ns){
+            returnArr.push(node);
+            if(node.nodeType === node.COMMENT_NODE && (node as Comment).data === closedText){
+                return returnArr;
+            }
+            ns = ns.nextSibling;
+        }
+    }else{
+        const el = node as Element;
+        const itemref = el.getAttribute('itemref');
+        if(itemref === null) return returnArr;
+        const itemrefList = splitRefs(itemref);// itemref.split(' ').map((id) => id.trim()).filter((id) => id.length > 0);
+        if(itemrefList.length === 0) return returnArr;
+        let ns = el.nextSibling;
+        while(ns !== null){
+            if(ns instanceof Element){
+                if(ns.id && itemrefList.includes(ns.id)){
+                    returnArr.push(ns);
+                }
+            }else{
+                return returnArr;
+            }
+            ns = ns.nextSibling;
+        }
+        
     }
+
     return returnArr;
 }
 
