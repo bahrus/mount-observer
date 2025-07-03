3377892c98f048a3cf86aa0e59f3f73431733e6e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-03 17:11:11 -0400

    0.0.73

--- slotkin/wrap.js
+++ slotkin/wrap.js
@@ -1,9 +1,9 @@
 export const wrapped = Symbol.for('50tzQZt95ECXUtHF7a40og');
-export function wrap(templ, base) {
+export function wrap(templ, base, force = false) {
     const wasWrapped = templ[wrapped];
     if (!wasWrapped) {
         templ[wrapped] = base;
-        if (templ.content.childElementCount > 1) {
+        if (force || templ.content.childElementCount > 1) {
             const start = document.createComment(base);
             templ.content.prepend(start);
             const end = document.createComment(`/${base}`);
--- slotkin/wrap.ts
+++ slotkin/wrap.ts
@@ -3,11 +3,12 @@ export const wrapped = Symbol.for('50tzQZt95ECXUtHF7a40og');
 export function wrap(
     templ: HTMLTemplateElement, 
     base: string,
+    force: boolean = false,
 ){
     const wasWrapped = (<any>templ)[wrapped];
     if (!wasWrapped) {
         (<any>templ)[wrapped] = base;
-        if (templ.content.childElementCount > 1) {
+        if (force || templ.content.childElementCount > 1) {
             const start = document.createComment(base);
             templ.content.prepend(start);
             const end = document.createComment(`/${base}`);
