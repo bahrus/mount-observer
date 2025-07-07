6eb0ea96fe95fa7da6fa68bc92dccdae59806d10
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-07 06:48:15 -0400

    0.0.75

--- slotkin/getBreadth.js
+++ slotkin/getBreadth.js
@@ -1,7 +1,7 @@
 export function getBreadth(innerEl, base) {
     const priors = [];
     let ps = innerEl.previousSibling;
-    while (ps !== null && !(ps.nodeType === Node.COMMENT_NODE && !ps.data.includes(` ${base} `))) {
+    while (ps !== null && !(ps.nodeType === Node.COMMENT_NODE && ps.data.includes(` ${base} `))) {
         priors.push(ps);
         ps = ps.previousSibling;
     }
@@ -9,7 +9,7 @@ export function getBreadth(innerEl, base) {
         priors.push(ps);
     const nexts = [];
     let ns = innerEl.nextSibling;
-    while (ns !== null && !(ns.nodeType === Node.COMMENT_NODE && !ns.data.includes(` /${base} `))) {
+    while (ns !== null && !(ns.nodeType === Node.COMMENT_NODE && ns.data.includes(` /${base} `))) {
         nexts.push(ns);
         ns = ns.nextSibling;
     }
--- slotkin/getBreadth.ts
+++ slotkin/getBreadth.ts
@@ -4,14 +4,14 @@ export function getBreadth(
 ){
     const priors: Array<Node> = [];
     let ps = innerEl.previousSibling as Comment | null;
-    while(ps !== null && !(ps.nodeType === Node.COMMENT_NODE && !(ps as Comment).data.includes( ` ${base} `))){
+    while(ps !== null && !(ps.nodeType === Node.COMMENT_NODE && (ps as Comment).data.includes( ` ${base} `))){
         priors.push(ps);
         ps = ps.previousSibling as Comment | null;
     }
     if(ps !== null) priors.push(ps);
     const nexts: Array<Node> = [];
     let ns = innerEl.nextSibling as Comment | null; 
-    while(ns !== null && !(ns.nodeType === Node.COMMENT_NODE && !(ns as Comment).data.includes( ` /${base} `))){
+    while(ns !== null && !(ns.nodeType === Node.COMMENT_NODE && (ns as Comment).data.includes( ` /${base} `))){
         nexts.push(ns);
         ns = ns.nextSibling as Comment | null;
     }
