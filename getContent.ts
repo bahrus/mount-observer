c7a6305acafa58078b8fc2d278b448ba7fb9b5df
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-26 08:39:59 -0400

    0.0.79

--- getContent.ts
+++ getContent.ts
@@ -1,7 +1,7 @@
 import {waitForEvent} from './waitForEvent.js';
 import {TemplateWithRemoteContent} from './ts-refs/mount-observer/types.js';
 
-export async function getContent(templ: HTMLTemplateElement, target?: DocumentFragment | ShadowRoot | Document | Element) {
+export async function getContent(templ: HTMLTemplateElement) {
     const templWithRemoteContent = templ as TemplateWithRemoteContent;
     if(templWithRemoteContent.remoteContent) return templWithRemoteContent.remoteContent;
     await waitForEvent(templ, 'load');
