6b0460c08b5dc6d2c12586067927511f96a68099
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-08-12 06:40:20 -0400

    0.0.89

--- waitForIsh.ts
+++ waitForIsh.ts
@@ -1,4 +1,4 @@
-export function waitForIsh(el: Element) : Promise<EventTarget> {
+export function waitForIsh(el: Element) : Promise<any> {
     return new Promise((resolve, reject) => {
         const ish = (<any>el)['ish']; // [TODO] should we make this something that can
         // be passed in, more generic function -- waitForProperty?
@@ -7,7 +7,7 @@ export function waitForIsh(el: Element) : Promise<EventTarget> {
         } else {
             // If the element is not yet defined, wait for it to be defined
             el.addEventListener('ish', () => {
-                const ish = (<any>el)['ish'] as EventTarget;
+                const ish = (<any>el)['ish'];
                 if (ish) {
                     resolve(ish);
                 } else {
