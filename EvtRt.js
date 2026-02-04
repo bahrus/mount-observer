1dc198e59e11bb7a0369cd17ad4f9f3529b8cf85
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-02-04 07:17:57 -0500

    Merge branch 'v2' of https://github.com/bahrus/mount-observer into v2

--- EvtRt.js
+++ EvtRt.js
@@ -1,24 +1,26 @@
 import { DismountEvent, MountEvent, DisconnectEvent, dismountEventName, disconnectEventName, mountEventName } from './Events';
 export class EvtRt {
     constructor(mountedElement, ctx) {
-        //this.#context = ctx;
-        const { observer } = ctx;
+        const { observer, mountInit } = ctx;
         const et = observer.getNotifier(mountedElement);
         et.addEventListener(mountEventName, this);
         et.addEventListener(disconnectEventName, this);
         et.addEventListener(dismountEventName, this);
-        this.mount(mountedElement, ctx);
+        this.mount(mountedElement, mountInit, ctx);
     }
-    mount(mountedElement, context) {
+    mount(mountedElement, mountInit, context) {
+        console.log({ mountedElement, context });
     }
     disconnect(mountedElement, mountInit) {
+        console.log({ mountedElement, mountInit });
     }
     dismount(mountedElement, mountInit) {
+        console.log({ mountedElement, mountInit });
     }
     handleEvent(evt) {
         if (evt instanceof MountEvent) {
-            const { matchingElement, mountContext } = evt;
-            this.mount(matchingElement, mountContext);
+            const { matchingElement, mountContext, mountInit } = evt;
+            this.mount(matchingElement, mountInit, mountContext);
         }
         else if (evt instanceof DismountEvent) {
             const { matchingElement, mountInit } = evt;
--- EvtRt.ts
+++ EvtRt.ts
@@ -6,32 +6,31 @@ import {
 } from './Events';
 export class EvtRt implements EventListenerObject{
     constructor(mountedElement: Element, ctx: MountContext ){
-        //this.#context = ctx;
-        const {observer} = ctx;
+        const {observer, mountInit} = ctx;
         const et = observer.getNotifier(mountedElement);
         et.addEventListener(mountEventName, this);
         et.addEventListener(disconnectEventName, this);
         et.addEventListener(dismountEventName, this);
-        this.mount(mountedElement, ctx);
+        this.mount(mountedElement, mountInit, ctx);
 
     }
 
-    mount(mountedElement: Element, context: MountContext){
-        
+    mount(mountedElement: Element, mountInit: MountInit, context: MountContext){
+        console.log({mountedElement, context});
     }
 
     disconnect(mountedElement: Element, mountInit: MountInit){
-        
+        console.log({mountedElement, mountInit});
     }
 
     dismount(mountedElement: Element, mountInit: MountInit){
-
+        console.log({mountedElement, mountInit});
     }
 
     handleEvent(evt: Event): void {
         if(evt instanceof MountEvent){
-            const {matchingElement, mountContext} = evt;
-            this.mount(matchingElement, mountContext);
+            const {matchingElement, mountContext, mountInit} = evt;
+            this.mount(matchingElement, mountInit, mountContext);
         }else if(evt instanceof DismountEvent){
             const {matchingElement, mountInit} = evt;
             this.dismount(matchingElement, mountInit);
